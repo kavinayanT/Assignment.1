54567d6e928d9962bfe275c3b447e7cf0d34a936
kavinayanT <kavinayan.thayananthakuamr@ontariotechu.net>
2024-10-23 22:47:14 -0400

    Initial commit

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-// Optional: Smooth scroll for internal links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-  });
-=======
-// Optional: Smooth scroll for internal links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-  });
->>>>>>> bdc3bc7542da96d1163573013414c63039d5c73b
-  
+function scrollToSection(section) {
+    const target = document.querySelector(section);
+    target.scrollIntoView({ behavior: "smooth" });
+}

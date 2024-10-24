00c0ab10fc13d70948058e26ccb2ff199926cc52
kavinayanT <kavinayan.thayananthakuamr@ontariotechu.net>
2024-10-23 21:46:14 -0400

    Initial commit with project files

---
+++ script.js
@@ -0,0 +1,10 @@
+// Optional: Smooth scroll for internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+  });
+  
